8460fc06c0302cd0a168a4bd5296bf50feb466d8
Riccardo <riccardo.gibello@mail.polimi.it>
2023-06-14 11:28:12 +0200

    Version 1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "62e32c8783eed10e06a8e096b25df3ae",
+  "assets/AssetManifest.json": "17597049aeb71109c83187210e3139a6",
 "assets/assets/attention.png": "effb3f2bd4fe650e9ad83d4fbd382089",
 "assets/assets/EMDN.csv": "c1b621822dc01e02c88d92264201dace",
+"assets/assets/european_commission_logo.png": "83e789615ef8b954ca400ec58ea15017",
+"assets/assets/gudid_logo_nobg.png": "19d5ee83d29593cf6ca9f91f88a6bf62",
 "assets/assets/Lato-Bold.ttf": "c3649582dc51f82ed9efa84cf666117b",
 "assets/assets/Lato-Regular.ttf": "4a1f31fb17b7b0686c929d4faba4be82",
 "assets/assets/logo.png": "c82a1b49a717fe24b8943338c938dace",
+"assets/assets/logo_polimi_scritta.png": "2a7907b5afc1567818c423f041ac924b",
+"assets/assets/ministero_logo.png": "484d18ccfaed11f014c3979be95146e2",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "305a09a1fdc5b24f6f1301bb8287f02f",
+"assets/NOTICES": "52670a5c98fb365e36087faa54df9336",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
@@ -23,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c6bb056a2dfb29a28de267fd68bd91cd",
-"/": "c6bb056a2dfb29a28de267fd68bd91cd",
-"main.dart.js": "cce7eab5fa64273f83dc5b1e821bc372",
+"index.html": "70d5de9beb360f7396aaa6663ff1b384",
+"/": "70d5de9beb360f7396aaa6663ff1b384",
+"main.dart.js": "4c6f64b0f58d43434f57480f2afa16e5",
 "manifest.json": "9f3935f1a69001070e292b98dd78819a",
 "version.json": "6b8863c5d7affe19f041d0a7f9042a06"
 };
